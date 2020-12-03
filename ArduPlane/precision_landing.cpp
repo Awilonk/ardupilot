936f147463752dbd10cf810c8828a790e31d28e6
luyaokun <luyaokun@didiglobal.com>
2020-12-03 20:58:27 +0800

    test

---
+++ ArduPlane/precision_landing.cpp
@@ -0,0 +1,28 @@
+//
+// functions to support precision landing
+//
+
+#include "Plane.h"
+
+//#if PRECISION_LANDING == ENABLED
+
+void Plane::init_precland()
+{
+    plane.precland.init(400);
+}
+
+
+void Plane::update_precland()
+{
+    int32_t height_above_ground_cm = current_loc.alt;
+
+    // use range finder altitude if it is valid, otherwise use home alt
+    if (rangefinder_alt_ok()) {
+        height_above_ground_cm = rangefinder_state.alt_cm;
+    }
+
+    precland.update(height_above_ground_cm, rangefinder_alt_ok());
+}
+
+
+//#endif
