60ccc6403b9e3c5572fb3631660661d07972ba58
luyaokun <luyaokun@didiglobal.com>
2020-12-03 20:35:49 +0800

    test

--- ArduCopter/precision_landing.cpp
+++ ArduCopter/precision_landing.cpp
@@ -11,11 +11,6 @@ void Copter::init_precland()
     copter.precland.init(400);
 }
 
-void Plane::init_precland()
-{
-    plane.precland.init(400);
-}
-
 
 void Copter::update_precland()
 {
@@ -30,15 +25,4 @@ void Copter::update_precland()
 }
 
 
-void Plane::update_precland()
-{
-    int32_t height_above_ground_cm = current_loc.alt;
-
-    // use range finder altitude if it is valid, otherwise use home alt
-    if (rangefinder_alt_ok()) {
-        height_above_ground_cm = rangefinder_state.alt_cm;
-    }
-
-    precland.update(height_above_ground_cm, rangefinder_alt_ok());
-}
 #endif
